9872f1b751422a0b2e4c4312b12bcbe70804ffdb
namphuongtran9196 <namphuongtran9196@gmail.com>
2022-04-26 21:53:12 +0700

    fix build generator model

--- src/models/generator.py
+++ src/models/generator.py
@@ -49,10 +49,8 @@ def build_generator(input_shape=(None,None,3)):
     
     # Residual block
     for i in range(8):
-        x = residual_block(input_shape=x.shap(2, 256, 256, 3)
-(2, 32, 32, 1)
-(2, 32, 32, 1)
-(2, 32, 32, 1)e='residual_block_'+str(i))(x)
+        x = residual_block(input_shape=x.shape[1:], filters=256, kernel_size=3, strides=1, 
+                           padding='same', name='residual_block_'+str(i))(x)
         
     # Upsampling block
     for i in range(1,3):
